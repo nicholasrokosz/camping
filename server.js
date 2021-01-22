8abc5ccdfa4764afa7e4c83eb1b20ce00f26ae4f
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-22 13:35:17 -0700

    fix models path

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const Todo = require("./models/Todo");
+const Todo = require('./models/User'); // <-- ERIK
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tododb", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tododb', {
+  // <-- ERIK
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // API ROUTES
+// <-- JACOB
 
-app.get("/api/todos", (req, res) => {
-  Todo.find({}).then((dbTodo) => res.json(dbTodo));
+app.get('/api/todos', (req, res) => {
+  Todo.find({}).then(dbTodo => res.json(dbTodo));
 });
 
-app.post("/api/todos", (req, res) => {
-  Todo.create(req.body).then((dbTodo) => res.json(dbTodo));
+app.post('/api/todos', (req, res) => {
+  Todo.create(req.body).then(dbTodo => res.json(dbTodo));
 });
 
 // END API ROUTES
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(__dirname + "/client/build/index.html");
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html');
   });
 }
 
