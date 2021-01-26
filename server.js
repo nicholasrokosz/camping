8acb44eef779e4149addb8f722572791e9bde13c
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-25 20:37:51 -0700

    add auth middleware

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://parkpal.us.auth0.com/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'https://parkpal.us.auth0.com/api/v2/',
+  issuer: `https://parkpal.us.auth0.com/`,
+  algorithms: ['RS256'],
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,21 +41,21 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/campdb', {
 // API ROUTES
 // <-- JACOB
 
-app.get('/api/users', (req, res) => {
-  User.find({}).then(data => res.json(data));
-});
+// app.get('/api/users', (req, res) => {
+//   User.find({}).then(data => res.json(data));
+// });
 
-app.post('/api/users', (req, res) => {
-  User.create(req.body).then(data => res.json(data));
-});
+// app.post('/api/users', (req, res) => {
+//   User.create(req.body).then(data => res.json(data));
+// });
 
-app.get('/api/campsites', (req, res) => {
-  Place.find({}).then(data => res.json(data));
-});
+// app.get('/api/campsites', (req, res) => {
+//   Place.find({}).then(data => res.json(data));
+// });
 
-app.post('/api/campsites', (req, res) => {
-  Place.create(req.body).then(data => res.json(data));
-});
+// app.post('/api/campsites', (req, res) => {
+//   Place.create(req.body).then(data => res.json(data));
+// });
 
 // END API ROUTES
 
