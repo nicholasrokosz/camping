32a2af7c36ba0db74129b5d1c750d7a72ec62daa
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-27 13:43:55 -0700

    remove unnecessary routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,5 @@
-const router = require('express').Router()
-const User = require('../models/User'); // <-- ERIK
-const Place = require('../models/Place'); // <-- ERIK
-
-router.get('/api/users', (req, res) => {
-  User.find({}).then(data => res.json(data));
-});
-
-router.post('/api/users', (req, res) => {
-  User.create(req.body).then(data => res.json(data));
-});
-
+const router = require('express').Router();
+const Place = require('../models/Place');
 
 router.get('/api/campsites', (req, res) => {
   Place.find({}).then(data => res.json(data));
@@ -19,4 +9,4 @@ router.post('/api/campsites', (req, res) => {
   Place.create(req.body).then(data => res.json(data));
 });
 
-module.exports = router
+module.exports = router;
