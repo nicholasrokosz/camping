65037dcf638cb789735b51a10a8fb5fde4bb8f50
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-27 14:22:10 -0700

    add user to schema

--- models/Place.js
+++ models/Place.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
+  user: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
