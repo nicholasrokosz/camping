c687f569ac7c89b80b39f516e4905a586123120e
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-27 14:00:09 -0700

    update placeSchema

--- models/Place.js
+++ models/Place.js
@@ -2,11 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const placeSchema = new Schema({
-  city: {
+  title: {
     type: String,
     required: true,
   },
-  coords: [{ longitude: Number, latitude: Number }],
+  rating: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  // coords: [{ longitude: Number, latitude: Number }],
 });
 
 const Place = mongoose.model('Place', placeSchema);
