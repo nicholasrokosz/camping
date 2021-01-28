6e2e4398694e818184b171aedeedfd5f42ea4c76
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-27 20:06:52 -0700

    remove app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-import "./App.css";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
   const { isLoading } = useAuth0();
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
