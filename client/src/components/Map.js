501f7576fe28a3823327dcf1065b12a1d1d7b652
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2021-01-28 20:52:24 -0700

    add working Popups

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -2,11 +2,11 @@ import { Attraction } from 'grommet-icons';
 import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import { useAuth0 } from '@auth0/auth0-react';
-import api from '../utils/api'
+import api from '../utils/api';
 
 function Map() {
   const { user, isAuthenticated } = useAuth0();
-  const [showPopup, togglePopup] = React.useState(true);
+  const [showPopup, togglePopup] = useState({});
   const [viewport, setViewport] = useState({
     latitude: 35.8283,
     longitude: -98.5795,
@@ -15,17 +15,16 @@ function Map() {
     zoom: 3.5,
     minzoom: 3.5,
   });
-  
-  const [allLocations, setAllLocations] = useState([])
+
+  const [allLocations, setAllLocations] = useState([]);
   const [campsite, setCampsite] = useState({
-    title: "",
-    rating: "",
-    user: user ? user.sub : "",
+    title: '',
+    rating: '',
+    user: user ? user.sub : '',
     latitude: 0,
-    longitude: 0
-  })
-  
-  
+    longitude: 0,
+  });
+
   const [newLocation, setNewLocation] = useState(null);
   const showNewMarker = e => {
     const [longitude, latitude] = e.lngLat;
@@ -37,32 +36,36 @@ function Map() {
     setCampsite({
       ...campsite,
       latitude: latitude,
-      longitude: longitude
-    })
+      longitude: longitude,
+    });
   };
-  
-  useEffect(() => {
-    api.loadCampsite().then(results => {
-      setAllLocations(results.data)
-    })
 
-  }, [])
-  
+  const loadSites = () => {
+    api.loadCampsite().then(results => {
+      setAllLocations(results.data);
+    });
+  };
+
+  useEffect(() => {
+    loadSites();
+  }, []);
+
   function handleInputChange(e) {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     setCampsite({
       ...campsite,
-      [name]: value
-    })
+      [name]: value,
+    });
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-  
+
     console.log('hellowWorld', campsite);
     api.saveCampsite(campsite).then(results => {
-      console.log(results)
-    })
+      console.log(results);
+    });
+    loadSites();
   }
 
   return (
@@ -72,27 +75,65 @@ function Map() {
         mapboxApiAccessToken='pk.eyJ1IjoidGhlY291bnRqcGMiLCJhIjoiY2trYTFlemZjMG02NTJ1cXc0ZXRwZzVtNCJ9.A-cvDeI0OcMzK_UpOhWS1A'
         mapStyle='mapbox://styles/thecountjpc/ckka2kjbs0w7t17plpz6hzqte'
         minZoom={3.5}
+        doubleClickZoom={false}
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
         onDblClick={showNewMarker}
       >
-         {console.log(allLocations)}
+        {console.log(allLocations)}
 
-         {allLocations.length > 0 ? allLocations.filter(campsite => {
-           return campsite.user === (user ? user.sub : "")
-         }).map(campsite => {
-           return (
-            <Marker  latitude={campsite.latitude}
-            longitude={campsite.longitude}>
-        <Attraction />
-      </Marker>
+        {allLocations.length > 0
+          ? allLocations
+              .filter(campsite => {
+                return campsite.user === (user ? user.sub : '');
+              })
+              .map(campsite => {
+                return (
+                  <>
+                    <Marker
+                      key={campsite._id}
+                      latitude={campsite.latitude}
+                      longitude={campsite.longitude}
+                      onClick={() => togglePopup({ [campsite._id]: true })}
+                    >
+                      <Attraction />
+                    </Marker>
+                    {showPopup[campsite._id] ? (
+                      <Popup
+                        latitude={campsite.latitude}
+                        longitude={campsite.longitude}
+                        closeButton={true}
+                        closeOnClick={false}
+                        dynamicPosition={true}
+                        onClose={() => togglePopup({})}
+                      >
+                        <div>
+                          <form
+                          // onSubmit= {handleSubmit}
+                          >
+                            <label htmlFor='title'>Title</label>
+                            <input
+                              name='title'
+                              onChange={handleInputChange}
+                              value={campsite.title}
+                            />
+                            <label htmlFor='rating'>Rating</label>
+                            <input
+                              name='rating'
+                              onChange={handleInputChange}
+                              value={campsite.rating}
+                            />
+                            <button onClick={handleSubmit}>Set</button>
+                          </form>
+                        </div>
+                      </Popup>
+                    ) : null}
+                  </>
+                );
+              })
+          : ''}
 
-
-           )
-         }): ""}
-        
-       
         {newLocation ? (
           <>
             <Marker
@@ -107,17 +148,26 @@ function Map() {
                 longitude={newLocation.longitude}
                 closeButton={true}
                 closeOnClick={false}
-                onClose={() => togglePopup(false)}
+                dynamicPosition={true}
+                onClose={() => setNewLocation(null)}
               >
                 <div>
                   <form
                   // onSubmit= {handleSubmit}
                   >
                     <label htmlFor='title'>Title</label>
-                    <input name='title' onChange={handleInputChange} value= {campsite.title} />
+                    <input
+                      name='title'
+                      onChange={handleInputChange}
+                      value={newLocation.title}
+                    />
                     <label htmlFor='rating'>Rating</label>
-                    <input name='rating' onChange= {handleInputChange} value= {campsite.rating} />
-                    <button onClick={handleSubmit}>Set </button>
+                    <input
+                      name='rating'
+                      onChange={handleInputChange}
+                      value={newLocation.rating}
+                    />
+                    <button onClick={handleSubmit}>Set</button>
                   </form>
                 </div>
               </Popup>
